1b0f953acb07930e81c77a4e5e56c5c408171853
Danilo Balarini <danilo.balarini@gmail.com>
2018-04-24 21:08:26 -0300

    thymeleaf working - more adjusts to come

--- src/main/kotlin/br/com/dblogic/blogkotlin/BlogkotlinApplication.kt
+++ src/main/kotlin/br/com/dblogic/blogkotlin/BlogkotlinApplication.kt
@@ -1,11 +1,29 @@
 package br.com.dblogic.blogkotlin
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
+@EnableWebMvc
 @SpringBootApplication
-class BlogkotlinApplication
-
+class BlogkotlinApplication : WebMvcConfigurer {
+	
+	private val log = LoggerFactory.getLogger(BlogkotlinApplication::class.java)
+	
+	override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+	
+		log.info(" ####### Pelo menos passou por aqui ####### ")
+		
+		registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/")
+		registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/")
+		registry.addResourceHandler("/javascript/**").addResourceLocations("classpath:/static/javascript/")
+	}
+	
+} 
+	
 fun main(args: Array<String>) {
     runApplication<BlogkotlinApplication>(*args)
 }
--- src/main/kotlin/br/com/dblogic/blogkotlin/controller/BlogController.kt
+++ src/main/kotlin/br/com/dblogic/blogkotlin/controller/BlogController.kt
@@ -1,29 +1,22 @@
 package br.com.dblogic.blogkotlin.controller
 
-import br.com.dblogic.blogkotlin.model.Post
-import br.com.dblogic.blogkotlin.service.PostService
-import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
-@RequestMapping("/post")
+@Controller
+@RequestMapping("", "/blog")
 class BlogController {
 	
-	@Autowired
-	lateinit var postService: PostService;
-	
-	@GetMapping("/findAll")
-	fun findAll(): List<Post> {
-		return postService.findAll();
+	@GetMapping("")
+	fun goHome() : String {
+		return "redirect:/index"
 	}
 	
-	@PostMapping("/create")
-	fun save(@RequestBody post: Post): Post {
-		return postService.save(post);
+	@GetMapping("/index")
+	fun index() : String {
+		return "index"
 	}
-	
+		
 }
