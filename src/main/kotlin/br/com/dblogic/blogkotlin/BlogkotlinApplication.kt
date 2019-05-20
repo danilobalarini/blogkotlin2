9695a76265d48f66861e7fe8b5f1fc338b352a3c
Danilo Balarini <danilo.balarini@gmail.com>
2019-05-20 18:31:02 -0300

    finally decided a design

--- src/main/kotlin/br/com/dblogic/blogkotlin/BlogkotlinApplication.kt
+++ src/main/kotlin/br/com/dblogic/blogkotlin/BlogkotlinApplication.kt
@@ -3,31 +3,12 @@ package br.com.dblogic.blogkotlin
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
-@EnableWebMvc
 @SpringBootApplication
-class BlogkotlinApplication : WebMvcConfigurer {
-	
-	private val log = LoggerFactory.getLogger(BlogkotlinApplication::class.java)
-	
-	override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
-	
-		log.info(" ####### Entering ResourceHandlers configurations ####### ")
-		
-		registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/")
-		registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/")
-		registry.addResourceHandler("/javascript/**").addResourceLocations("classpath:/static/javascript/")
-		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/")
-		
-		// ## swagger config ##
-		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-	}
-	
-}
+class BlogkotlinApplication
 
 fun main(args: Array<String>) {
-    runApplication<BlogkotlinApplication>(*args)
+	runApplication<BlogkotlinApplication>(*args)
 }
---
+++ src/main/kotlin/br/com/dblogic/blogkotlin/config/WebConfiguration.kt
@@ -0,0 +1,28 @@
+package br.com.dblogic.blogkotlin.config
+
+import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+@EnableWebMvc
+class WebConfiguration : WebMvcConfigurer {
+	
+	private val log = LoggerFactory.getLogger(WebConfiguration::class.java)
+	
+	override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
+	
+		log.info(" ####### Entering ResourceHandlers configurations ####### ")
+		
+		registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/")
+		registry.addResourceHandler("/images/**").addResourceLocations("classpath:/static/images/")
+		registry.addResourceHandler("/javascript/**").addResourceLocations("classpath:/static/javascript/")
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/")
+		
+		// ## swagger config ##
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+	}
+	
+}
