00f94159b3b19d095a1418706f4fe1a7d5a6ea60
Danilo <danilo.balarini@gmail.com>
2019-09-25 17:26:57 -0300

    cleaning html, defining composer functions

--- src/main/kotlin/br/com/dblogic/blogkotlin/BlogkotlinApplication.kt
+++ src/main/kotlin/br/com/dblogic/blogkotlin/BlogkotlinApplication.kt
@@ -1,10 +1,7 @@
 package br.com.dblogic.blogkotlin
 
-import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 class BlogkotlinApplication
